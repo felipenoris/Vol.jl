e3c67cceaf8396835a2349283527097ccaa0671a
Felipe Noronha <felipenoris@gmail.com>
2018-03-29 19:53:17 -0300

    dummy test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,4 @@ return_series = 0.01 * collect(1:50)
 lambda = 0.94
 
 @test isapprox(Vol.ewma(return_series, lambda), sqrt(Vol.ewma_canonical(return_series.^2, lambda)))
+@test isapprox(Vol.ewma(0.01 * ones(100), lambda), 0.01)
